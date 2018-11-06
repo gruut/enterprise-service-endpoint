0b224322f522b8b8c220ed5c81bbcff7d6935682
Junsuk Park <park64kr63@gmail.com>
2018-11-06 15:05:15 +0900

    CONFIG - Project

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,47 +6,43 @@
 </template>
 
 <script>
-import MyFooter from '~/components/Footer.vue'
+  import Footer from '~/components/Footer.vue'
 
-export default {
-  components: {
-    MyFooter
+  export default {
+    components: {
+      Footer
+    }
   }
-}
 </script>
 
 <style>
-.container
-{
-  margin: 0;
-  width: 100%;
-  padding: 100px 0;
-  text-align: center;
-}
+  .container {
+    margin: 0;
+    width: 100%;
+    padding: 100px 0;
+    text-align: center;
+  }
 
-.button, .button:visited
-{
-  display: inline-block;
-  color: black;
-  letter-spacing: 1px;
-  background-color: #fff;
-  border: 2px solid #000;
-  text-decoration: none;
-  text-transform: uppercase;
-  padding: 15px 45px;
-}
+  .button, .button:visited {
+    display: inline-block;
+    color: black;
+    letter-spacing: 1px;
+    background-color: #fff;
+    border: 2px solid #000;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 15px 45px;
+  }
 
-.button:hover, .button:focus
-{
-  color: #fff;
-  background-color: #000;
-}
+  .button:hover, .button:focus {
+    color: #fff;
+    background-color: #000;
+  }
 
-.title
-{
-  color: #000;
-  font-weight: 300;
-  font-size: 2.5em;
-  margin: 0;
-}
+  .title {
+    color: #000;
+    font-weight: 300;
+    font-size: 2.5em;
+    margin: 0;
+  }
 </style>
