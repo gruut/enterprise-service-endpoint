882cf7c23b4753d70598e602c3be70afe2fbfe8b
Junsuk Park <park64kr63@gmail.com>
2018-12-17 11:17:16 +0900

    # EDIT - CONFIG 및 필요없는 lib 제거

--- services/tx_generator.js
+++ services/tx_generator.js
@@ -20,8 +20,7 @@ class TxGenerator {
     const packageDefinition = protoLoader.loadSync(TX_PROTO_PATH, LOAD_ARGS)
     const ProtoTransaction = grpc.loadPackageDefinition(packageDefinition).grpc_se
 
-    // TODO: config 파일
-    const remoteServerAddr = '10.10.10.108:50051'
+    const remoteServerAddr = `${process.env.MERGER_ADDRESS}:${process.env.MERGER_PORT}`
     this.client = new ProtoTransaction.GruutSeService(remoteServerAddr, grpc.credentials.createInsecure())
   }
 
@@ -85,23 +84,7 @@ class TxGenerator {
     })
     const sigBuffer = Buffer.concat(bufferList)
 
-    // TODO: config
-    const privateKey = '-----BEGIN RSA PRIVATE KEY-----\n' +
-      'MIICXQIBAAKBgQDCtTEic76GBqUetJ1XXrrWZcxd8vJr2raWRqBjbGpSzLqa3YLv\n' +
-      'VxVeK49iSlI+5uLX/2WFJdhKAWoqO+03oH4TDSupolzZrwMFSylxGwR5jPmoNHDM\n' +
-      'S3nnzUkBtdr3NCfq1C34fQV0iUGdlPtJaiiTBQPMt4KUcQ1TaazB8TzhqwIDAQAB\n' +
-      'AoGAM8WeBP0lwdluelWoKJ0lrPBwgOKilw8W0aqB5y3ir5WEYL1ZnW5YXivS+l2s\n' +
-      'tNELrEdapSbE9hieNBCvKMViABQXj4DRw5Dgpfz6Hc8XIzoEl68DtxL313EyouZD\n' +
-      'jOiOGWW5UTBatLh05Fa5rh0FbZn8GsHrA6nhz4Fg2zGzpyECQQDi8rN6qhjEk5If\n' +
-      '+fOBT+kjHZ/SLrH6OIeAJ+RYstjOfS0bWiM9Wvrhtr7DZkIUA5JNsmeANUGlCrQ2\n' +
-      'cBJU2cJJAkEA26HyehCmnCkCjit7s8g3MdT0ys5WvrAFO6z3+kCbCAsGS+34EgnF\n' +
-      'yz8dDdfUYP410R5+9Cs/RkYesqindsvEUwJBALCmQVXFeKnqQ99n60ZIMSwILxKn\n' +
-      'Dhm6Tp5Obssryt5PSQD1VGC5pHZ0jGAEBIMXlJWtvCprScFxZ3zIFzy8kyECQQDB\n' +
-      'lUhHVo3DblIWRTVPDNW5Ul5AswW6JSM3qgkXxgHfYPg3zJOuMnbn4cUWAnnq06VT\n' +
-      'oHF9fPDUW9GK3yRbjNaJAkAB2Al6yY0KUhYLtWoEpQ40HlATbhNel2cn5WNs6Y5F\n' +
-      '2hedvWdhS/zLzbtbSlOegp00d2/7IBghAfjAc3DE9DZw\n' +
-      '-----END RSA PRIVATE KEY-----'
-
+    const privateKey = process.env.PRIVATE_KEY
     const signer = crypto.createSign('sha256')
     signer.update(sigBuffer)
     return signer.sign(privateKey, 'base64')
