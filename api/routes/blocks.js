8d8c09b3b9f52fb9b22b8c4fd9741c4e15403411
Junsuk Park <park64kr63@gmail.com>
2018-12-17 11:40:25 +0900

    # ADD - MSG_HEADER 처리하도록 router 추가

--- api/routes/blocks.js
+++ api/routes/blocks.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
 const {Block} = require('../../models')
+const bodyParser = require('body-parser')
+const debug = require('debug')('app:demo')
 const router = Router()
 
 /* GET Blocks listing. */
@@ -26,6 +28,18 @@ router.get('/blocks/:id', async (req, res) => {
     if (block) res.json(block)
 
     res.sendStatus(404)
+  } catch (err) {
+    res.sendStatus(500)
+    throw err
+  }
+})
+
+/* POST handle MSG_HEADER */
+router.post('/blocks', bodyParser.json(), (req, res) => {
+  try {
+    // TODO: save block
+    debug(req.body)
+    res.sendStatus(200)
   } catch (err) {
     res.sendStatus(500)
     throw err
