9cc42256e100f1706627d130edd5bfd71bd86599
Junsuk Park <park64kr63@gmail.com>
2018-12-28 17:45:08 +0900

    GEN-200 # EDIT - block이 새로 생성될때마다 table 업데이트하도록 수정

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,303 @@
 <template>
-  <block-list :blocks="blocks.slice(0, 8)" :show_more="true"></block-list>
+  <v-card class="explorer_body">
+    <v-card-title class="explorer_body__table_title">
+      <span class="explorer_body__table_title_text">BlockChain Explorer</span>
+      <v-text-field
+        class="explorer_body__table_search"
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="blocks"
+      :search="search"
+      :pagination.sync="pagination"
+      :rows-per-page-items="rowsPerPageItems"
+      class="elevation-1 explorer_body__table"
+    >
+      <template
+        slot="headerCell"
+        slot-scope="{ header }"
+      >
+        <span class="subheading font-weight-bold explorer_body__table_cell--header" v-text="header.text"></span>
+      </template>
+      <template slot="items" slot-scope="{ item }">
+        <td :class="{'explorer_body__table_cell--new': item.isActive}">
+          <nuxt-link :to="{ name: 'blocks-id', params: { id: item.id }}"
+                     class="explorer_body__table_cell explorer_body__table_cell--link">
+            {{ item.blockId }}
+          </nuxt-link>
+        </td>
+        <td :class="{'explorer_body__table_cell--new': item.isActive}" class="text-xs-center">{{ item.version }}</td>
+        <td :class="{'explorer_body__table_cell--new': item.isActive}" class="text-xs-center">{{ item.time }}</td>
+        <td :class="{'explorer_body__table_cell--new': item.isActive}" class="text-xs-center">{{ item.height }}</td>
+      </template>
+      <v-alert slot="no-results" :value="true" color="error" icon="error">
+        Your search for "{{ search }}" found no results.
+      </v-alert>
+
+      <template slot="no-data">
+        <v-alert :value="true" icon="warning">
+          Sorry, nothing to display
+        </v-alert>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
   import axios from '~/plugins/axios'
-  import BlockList from '~/components/BlockList.vue'
-  const moment = require('moment-timezone')
+  const moment = require('moment')
+  const _ = require('partial-js')
 
   export default {
-    head () {
-      return {
-        title: 'Gruut Enterprise Network'
+    name: 'blocks',
+    filters: {
+      truncate: (value, size, suffix = '...') => {
+        return value.substring(0, size) + suffix
       }
     },
-
     data () {
       return {
-        blocks: []
+        blocks: [],
+        isFirstLoading: true,
+        timer: '',
+        tries: 0,
+        search: '',
+        pagination: {
+          sortBy: 'height',
+          descending: true
+        },
+        rowsPerPageItems: [10, 20, 30, 40],
+        headers: [
+          {
+            text: 'Block ID',
+            align: 'center',
+            sortable: false,
+            value: 'blockId'
+          },
+          {
+            text: 'Block Version',
+            sortable: false,
+            align: 'center',
+            value: 'version'
+          },
+          {
+            text: 'Block 생성시간',
+            sortable: true,
+            align: 'center',
+            value: 'createdAt'
+          },
+          {
+            text: 'Block Height',
+            sortable: true,
+            align: 'center',
+            value: 'height'
+          }
+        ]
       }
     },
+    async created () {
+      this.blocks = await this.fetchBlocks()
+      this.timer = setInterval(this.checkNewBlock, 5000)
+    },
+    methods: {
+      async fetchBlocks () {
+        let {data} = await axios.get('/api/blocks')
+        data.forEach(block => {
+          block.time = moment(block.time).format('MMMM Do YYYY, h:mm:ss a')
+        })
 
-    async asyncData () {
-      let {data} = await axios.get('/api/blocks')
-      data.forEach(block => {
-        block.time = moment.tz(block.time, 'Asia/Seoul').format('MMMM Do YYYY, h:mm:ss a')
-      })
-      return {blocks: data}
+        return data
+      },
+      checkNewBlock: async function () {
+        const newBlocks = await this.fetchBlocks()
+        _.go(newBlocks,
+          (newBlocks) => {
+            const oldBlockHeights = _.map(this.blocks, (d) => d.height)
+            const newBlockHeights = _.map(newBlocks, (d) => d.height)
+
+            return _.difference(newBlockHeights, oldBlockHeights)
+          },
+          (newHeights) => {
+            _.each(newHeights, (h) => {
+              const newBlockIndex = _.findIndex(newBlocks, (b) => {
+                return b.height === h
+              })
+
+              if (newBlockIndex !== -1) {
+                newBlocks[newBlockIndex].isActive = true
+                this.blocks = newBlocks
+              }
+            })
+          }
+        )
+      },
+      cancelAutoUpdate: function () { clearInterval(this.timer) }
     },
 
-    components: {
-      'block-list': BlockList
+    beforeDestroy () {
+      this.cancelAutoUpdate()
     }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  $green: #00937B;
+  $break-small: 600px;
 
+  .explorer_body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #ffffff;
+    padding-bottom: 2rem;
+  }
+
+  .explorer_body__table {
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    font-family: "Ubuntu", sans-serif;
+  }
+
+  .explorer_body__table_row {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 80%;
+    background-color: white;
+    padding: 1rem 2rem;
+  }
+
+  .explorer_body__table_row:nth-child(even) {
+    background-color: rgb(248, 248, 248);
+  }
+
+  .explorer_body__table_row:nth-child(1) {
+    background-color: $green;
+  }
+
+  .explorer_body__table_cell {
+    @media screen and (max-width: $break-small) {
+      font-size: 0.6rem;
+    }
+  }
+
+  .explorer_body__table_cell--link {
+    color: $green;
+    text-decoration: none;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .explorer_body__blocks_index {
+    display: block;
+    text-decoration: none;
+    margin-top: 2rem;
+    text-align: center;
+  }
+
+  .explorer_body__blocks_button:hover {
+    background-color: $green;
+    color: #ffffff;
+    cursor: pointer;
+  }
+
+  .explorer_body__blocks_button {
+    outline: none;
+    height: 40px;
+    text-align: center;
+    width: 130px;
+    border-radius: 40px;
+    background: #fff;
+    border: 2px solid $green;
+    color: $green;
+    letter-spacing: 1px;
+    text-shadow: none;
+    font: {
+      size: 1rem;
+      weight: bold;
+    }
+    cursor: pointer;
+    transition: all 0.25s ease;
+
+    &:hover {
+      color: white;
+      background: $green;
+    }
+
+    &:active {
+      //letter-spacing: 2px;
+      letter-spacing: 2px;
+    }
+
+    @media screen and (max-width: $break-small) {
+      width: 100px;
+      height: 30px;
+      font-size: 0.8rem;
+    }
+  }
+
+  .explorer_body__table_cell--header {
+    font-size: 1.2rem;
+
+    @media screen and (max-width: $break-small) {
+      font-size: 0.8rem;
+    }
+  }
+
+  .explorer_body__table_cell--time {
+    font-size: 1rem;
+    flex-grow: 3;
+    flex-basis: 0;
+
+    @media screen and (max-width: $break-small) {
+      font-size: 0.8rem;
+      text-align: center;
+    }
+  }
+
+  .explorer_body__table_title {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    width: 85%;
+  }
+
+  .explorer_body__table_title_text {
+    color: $green;
+    font-size: 1.7rem;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 600;
+
+    @media screen and (max-width: $break-small){
+      font-size: 1.5rem;
+    }
+  }
+
+  .explorer_body__table_search {
+    color: $green;
+    flex-grow: 0.3;
+  }
+
+  @keyframes newBlock {
+    from {background-color: rgba($green, 0.5)}
+    to {background-color: transparent}
+  }
+  .explorer_body__table_cell--new {
+    animation: newBlock 3s;
+  }
 </style>
--- plugins/my_utils.js
+++ plugins/my_utils.js
@@ -4,6 +4,87 @@
 // Buffer encoding, Hash
 // https://nodejs.org/docs/latest/api/buffer.html#buffer_class_method_buffer_from_string_encoding
 const protobuf = require("protobufjs");
+const lz4 = require('lz4')
+const crypto = require('crypto')
+
+const HEADER_LENGTH = 32;
+
+const pack = function (MSG_TYPE, data, sender_id) {
+  let zip_data = this.zipIt(data);
+  let header = buildHeader(MSG_TYPE, zip_data, sender_id);
+  let mac = buildHMAC(zip_data.body);
+  return Buffer.concat([header, zip_data.body, mac]);
+};
+
+const zipIt = function (data){
+  let str_data = JSON.stringify(data);
+  let input = new Buffer.from(str_data);
+  let output = new Buffer.allocUnsafe( lz4.encodeBound(str_data.length) );
+  let compressedSize = lz4.encodeBlock(input, output);
+  output = output.slice(0, compressedSize);
+  var zip_data = {
+    body : output,
+    length : compressedSize
+  };
+
+  return zip_data;
+};
+
+// build front 6 bytes of the header
+const headerFront = function (type_byte){
+  return new Buffer.from([0x47 // 'G'
+    ,0x11 // major minor
+    ,type_byte
+    ,0x01 // MAC -> ECDSA
+    ,0x04 // zip -> lz4
+    ,0x00 // not used
+  ]);
+};
+
+const headerLength = function (length){
+  let buf = Buffer.allocUnsafe(4);
+  buf.writeInt32BE( (length + HEADER_LENGTH), 0);
+  return buf;
+};
+
+const headerChainId = function (id){
+  let buf = Buffer.allocUnsafe(8).fill(0);
+  buf.writeInt32BE(1, 4);
+  return buf;
+};
+
+const headerSender = function (sender_id){
+  if (sender_id.length > 16) return null;
+
+  return new Buffer.from(sender_id, 'hex');
+};
+
+const headerReserved = function (){
+  return Buffer.allocUnsafe(6).fill(0);
+};
+
+const buildHeader = function (type_byte, zip_data, sender_id){
+  var head = {
+    front : headerFront(type_byte),
+    total_length : headerLength(zip_data.length),
+    chainid : headerChainId(1),
+    sender : headerSender(sender_id),
+    reserved : headerReserved()
+  };
+
+  return Buffer.concat([head.front, head.total_length, head.chainid, head.sender, head.reserved], HEADER_LENGTH);
+};
+
+var getHMAC = function(data){
+  const secret = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  return crypto.createHmac('sha256', Buffer.from(secret, 'hex'))
+    .update(data)
+    .digest('hex');
+}
+
+const buildHMAC = function (zip_data){
+  return new Buffer.from(getHMAC(zip_data), 'hex');
+};
 
 const protobuf_msg_serializer = function(PROTO_PATH, msg_type_name, packed_msg){
   const root = protobuf.loadSync(PROTO_PATH);
@@ -24,6 +105,8 @@ const getTimestamp = function(){
 };
 
 const self = module.exports = {
+  pack : pack,
+  zipIt: zipIt,
   protobuf_msg_serializer : protobuf_msg_serializer,
   getTimestamp : getTimestamp
 };
